755bac0f24343e3e61bcbab7d3d5bbb37a0b1d76
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-12-15 12:15:04 -0300

    exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60) { //condição simples
+    console.log(`Vc ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`Dirija sempre usando o cinto`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'frnça'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    console.log(`VC é Brasileiro`)
+} else {
+    console.log(`VC é Estrangeiro`)
+}
