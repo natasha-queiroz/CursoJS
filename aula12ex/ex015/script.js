ec7efbb5faec417fb60c7fc87dfb7bc35df25ff9
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2025-01-19 18:59:50 -0300

    atualização

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,18 +1,20 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear() // ano atual com 4 digitos
-    var fano = document.getElementById('txtano') // formulario que veio quando quando preencheu
+    var fano = document.getElementById('txtano') // formulario que veio quando quando preencheu(txtano)
     var res = document.querySelector('div#res')
 
     if (fano.value.length == 0 || fano.value > ano) {
         alert('[ERRO] verifique os dados e tente novamente ')
     } else {
-        var fsex = document.getElementsByName('radsex')
+        var fsex = document.getElementsByName('radsex') //sexo
         var idade = ano - fano.value
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        if(fsex[0].checked) {
+        //dados do formulario preenchido
+
+        if(fsex[0].checked) { // se o que tiver marcado (checado) for o fsex[0], sera masculino
             genero = 'Homem'
             if(idade >=0 && idade < 10) {
                 //crianca
@@ -27,7 +29,7 @@ function verificar() {
                 //idoso
                 img.setAttribute('src', 'idosohomem.jpg')
             }
-        } else if (fsex[1].checked) {
+        } else if (fsex[1].checked) { // se o que tiver marcado (checado) for o fsex[1], sera feminino
             genero = 'Mulher'
             if(idade >=0 && idade < 10) {
                 //crianca
@@ -43,7 +45,7 @@ function verificar() {
                 img.setAttribute('src', 'idosamulher.jpg')
             }
         }
-        res.style.textAlign = 'center'
+        res.style.textAlign = 'center' //stilo pra centralizar 
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         res.appendChild(img)
     }
