19ca4d1d122cef016e65988837e1ac6b41dbcb0a
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2025-03-19 19:09:47 -0300

    js - A011

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,8 +1,21 @@
-var vel = 78.2
-console.log(`a velocidade do seu carro é ${vel}km/h`)
+// var vel = 50
+// console.log(`a velocidade do seu carro é ${vel}km/h`)
 
-if(vel > 60) { //condição simples
-    console.log(`Vc ultrapassou a velocidade permitida. Multado!`)
+// if(vel > 60) { //condição simples
+//     console.log(`Vc ultrapassou a velocidade permitida. Multado!`)
+// }
+
+// console.log(`Dirija sempre usando o cinto`)
+
+var idade = 68
+console.log(`vc tem ${idade} anos`)
+if (idade < 16) {
+    console.log('Nao vota')    
+} else {
+    if (idade >= 16 && idade < 18 || idade > 65) {
+        console.log('voto opcional')
+    } else {
+        console.log('VOto obrigatorio')
+
+    } 
 }
-
-console.log(`Dirija sempre usando o cinto`)
