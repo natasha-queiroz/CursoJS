1a79a81480effa536de06047b61bef8f82bc33e5
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2025-03-25 20:21:42 -0300

    ex13

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c =1
+do  {
+    console.log(`passo ${c} `)
+    c++ //c+1
+} while (c <= 6)
+
+// var c =1
+// while (c <= 6) {
+//     console.log(`passo ${c}?`)
+//     c++ //c+1
+// }
+
+
+
+// console.log('tudo bem?')
+
